89dc3b38968c1cb79763f21076e36ef75fd832c6
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-14 13:35:38 +0900

    보건소, 보건지소 찾기

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -2,3 +2,8 @@ select hospital_name, road_name_address
 from bbs.nation_wide_hospitals
 where road_name_address like "경기도 수원시%"
   and hospital_name like "%피부과%"
+
+
+SELECT business_type_name, hospital_name, road_name_address
+FROM bbs.nation_wide_hospitals
+where business_type_name in ('보건소', '보건지소');
