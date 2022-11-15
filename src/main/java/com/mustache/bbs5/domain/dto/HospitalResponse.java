a22f8ab752a71c066c9db01453ee6beb6c2fa94b
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-15 15:46:37 +0900

    restControllerTest추가. jsonResponse

--- src/main/java/com/mustache/bbs5/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbs5/domain/dto/HospitalResponse.java
@@ -1,11 +1,14 @@
 package com.mustache.bbs5.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class HospitalResponse {
     private Integer id;
     private String address;
