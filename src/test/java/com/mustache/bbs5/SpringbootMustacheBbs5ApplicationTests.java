5942ec0658316d76d247b9eee98a690e135ed4e8
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-12-07 13:50:03 +0900

    gitignore update yml

--- src/test/java/com/mustache/bbs5/SpringbootMustacheBbs5ApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.mustache.bbs5;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringbootMustacheBbs5ApplicationTests {
-
-}
