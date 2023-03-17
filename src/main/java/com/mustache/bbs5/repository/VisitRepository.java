cb84e80073f25ecb656a31ed2d68d224f053e411
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2023-03-17 17:24:13 +0900

    - commit after remove git cached

--- src/main/java/com/mustache/bbs5/repository/VisitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mustache.bbs5.repository;
-
-import com.mustache.bbs5.domain.Visit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VisitRepository extends JpaRepository<Visit, Long> {
-
-}
